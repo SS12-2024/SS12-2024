454999be75d0f4bce0a6fe3d8fedc1965ad206f4
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 13:27:52 -0800

    Added new screens to Navigator

--- navigation/GameNavigator.jsx
+++ navigation/GameNavigator.jsx
@@ -3,13 +3,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LeaderboardScreen from '../screens/LeaderboardScreen'
 import MenuScreen from '../screens/MenuScreen';
 import GameScreen from '../screens/GameScreen';
+import SplashScreen from '../screens/SplashScreen';
+import SignInScreen from '../screens/SignInScreen';
+import TutorialScreen from '../screens/TutorialScreen';
+import LevelScreen from '../screens/LevelScreen';
 
 const Stack = createNativeStackNavigator();
 
 const GameNavigator = () => {
     return (
-        <Stack.Navigator initialRouteName="Menu">
+        <Stack.Navigator initialRouteName="Splash">
+            <Stack.Screen name="Splash" component={SplashScreen} />
+            <Stack.Screen name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="Tutorial" component={TutorialScreen} />
             <Stack.Screen name="Menu" component={MenuScreen} />
+            <Stack.Screen name="Level" component={LevelScreen} />
             <Stack.Screen name="Leaderboard" component={LeaderboardScreen} />
             <Stack.Screen name="Gameplay" component={GameScreen} />
             {/* Add more screens as needed */}
