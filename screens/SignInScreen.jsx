30bcd3c56ec824b9485c17df7a7b1579fa136a52
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 15:26:40 -0800

    SignInScreen button fills entire screen

--- screens/SignInScreen.jsx
+++ screens/SignInScreen.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { View, Button, Text, AccessibilityInfo } from 'react-native';
 import { styles } from './styles/MenuStyles.js'
 import CustomButton from '../components/common/CustomButton';
+import { appStyles } from './styles/appStyles.js';
 
 const SignInScreen = ({ navigation }) => {
     // Announce screen changes for screen readers
@@ -11,8 +12,7 @@ const SignInScreen = ({ navigation }) => {
     }, []);
 
     return (
-        <View accessible={true} accessibilityLabel="Gameplay Screen">
-            <Text>Sign in Screen</Text>
+        <View accessible={true} accessibilityLabel="Gameplay Screen" style={appStyles.container}>
             <CustomButton
                 title="Go to Tutorial"
                 onPress={() => navigation.navigate('Tutorial')}
