750268c7d295100e6397f506ebacf5e0272edb5a
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 00:52:23 -0800

    added node_modules

--- screens/Account.tsx
+++ screens/Account.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import {View, Text, StyleSheet} from 'react-native'
-import ProfilePicture from "./profilepic";
 
 function AccountScreen(props: any) {
-<ProfilePicture imageUrl="c:\Users\Ms833704\AppData\Local\Temp\Untitled.jpg"/>
 
     return (
         <View style= {styles.container}>
         <Text style= {styles.accountname} >
-            
         </Text>
         </View>
 
