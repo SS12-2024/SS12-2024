aa373cb8eacf99591dde9d57d513f455b4a2a332
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 12:32:06 -0800

    Added sign in screen

---
+++ screens/SignInScreen.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { View, Button, Text, AccessibilityInfo } from 'react-native';
+
+const SignInScreen = ({ navigation }) => {
+    // Announce screen changes for screen readers
+    useEffect(() => {
+        const screenChangeAnnouncement = "Gameplay Screen.";
+        AccessibilityInfo.announceForAccessibility(screenChangeAnnouncement);
+    }, []);
+
+    return (
+        <View accessible={true} accessibilityLabel="Gameplay Screen">
+            <Text>Here will be gameplay</Text>
+        </View>
+    );
+};
+
+export default SignInScreen;
