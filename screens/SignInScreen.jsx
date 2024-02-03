d17a668393f39b7a07fba17d6132325bffc58db1
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 13:28:32 -0800

    Added SignInScreen

--- screens/SignInScreen.jsx
+++ screens/SignInScreen.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { View, Button, Text, AccessibilityInfo } from 'react-native';
+import { styles } from './styles/MenuStyles.js'
+import CustomButton from '../components/common/CustomButton';
 
 const SignInScreen = ({ navigation }) => {
     // Announce screen changes for screen readers
@@ -10,7 +12,14 @@ const SignInScreen = ({ navigation }) => {
 
     return (
         <View accessible={true} accessibilityLabel="Gameplay Screen">
-            <Text>Here will be gameplay</Text>
+            <Text>Sign in Screen</Text>
+            <CustomButton
+                title="Go to Tutorial"
+                onPress={() => navigation.navigate('Tutorial')}
+                buttonStyle={styles.button}
+                textStyle={{ color: 'white', fontSize: 50 }}
+                accessibilityLabel="Start tutorial"
+            />
         </View>
     );
 };
