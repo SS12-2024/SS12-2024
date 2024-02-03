207ba7af81c2b159301251264d050f7ea1318d17
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 15:27:07 -0800

    Splash screen button fills entire screen

--- screens/SplashScreen.jsx
+++ screens/SplashScreen.jsx
@@ -1,61 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import * as Speech from 'expo-speech';
+import React, { useEffect } from 'react';
+import { View, Button, Text, AccessibilityInfo } from 'react-native';
+import { styles } from './styles/MenuStyles.js'
+import CustomButton from '../components/common/CustomButton';
+import { appStyles } from './styles/appStyles.js';
 
-// Constant pitch value
-const CONSTANT_PITCH = 1.2;
+const SplashScreen = ({ navigation }) => {
+    // Announce screen changes for screen readers
+    useEffect(() => {
+        const screenChangeAnnouncement = "Splash Screen.";
+        AccessibilityInfo.announceForAccessibility(screenChangeAnnouncement);
+    }, []);
 
-export default function App() {
-  // Function to speak the given text with a constant pitch
-  const speakWithConstantPitch = (text) => {
-    Speech.speak(text, { language: 'en', pitch: CONSTANT_PITCH });
-  };
+    return (
+        <View accessible={true} accessibilityLabel="Gameplay Screen" style={appStyles.container}>
+            <CustomButton
+                title="Sign In"
+                onPress={() => navigation.navigate('SignIn')}
+                buttonStyle={styles.button}
+                textStyle={{ color: 'white', fontSize: 50 }}
+                accessibilityLabel="Start a new game"
+            />
+        </View>
+    );
+};
 
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.startButton}
-        onPress={() => speakWithConstantPitch('Start a new game')}>
-        <Text style={styles.buttonText}>Start a new game</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.leaderboardButton}
-        onPress={() => speakWithConstantPitch('Leadership Board')}>
-        <Text style={styles.buttonText}>Leadership Board</Text>
-      </TouchableOpacity>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    paddingHorizontal: 16,
-  },
-  startButton: {
-    flex: 1,
-    backgroundColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 12,
-    borderRadius: 8,
-    marginRight: 8,
-  },
-  leaderboardButton: {
-    flex: 1,
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 12,
-    borderRadius: 8,
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center',
-  },
-});
+export default SplashScreen;
