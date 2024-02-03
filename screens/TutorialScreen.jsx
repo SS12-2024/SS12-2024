a891ab7f711ddea4a6420d820d5ad8b5564dc03d
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 13:29:12 -0800

    Added tutorial

--- screens/TutorialScreen.jsx
+++ screens/TutorialScreen.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { View, Button, Text, AccessibilityInfo } from 'react-native';
+import { styles } from './styles/MenuStyles.js'
+import CustomButton from '../components/common/CustomButton';
 
 const TutorialScreen = ({ navigation }) => {
     // Announce screen changes for screen readers
@@ -11,6 +13,13 @@ const TutorialScreen = ({ navigation }) => {
     return (
         <View accessible={true} accessibilityLabel="Tutorial Screen">
             <Text>Here will be Tutorial</Text>
+            <CustomButton
+                title="Go to Menu Screen"
+                onPress={() => navigation.navigate('Menu')}
+                buttonStyle={styles.button}
+                textStyle={{ color: 'white', fontSize: 50 }}
+                accessibilityLabel="Go to Menu"
+            />
         </View>
     );
 };
