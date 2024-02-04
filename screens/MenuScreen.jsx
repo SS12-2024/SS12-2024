d813dbd3f26a8598692f59bbe9cf8f93d74dc22c
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 17:37:43 -0800

    Improved code

--- screens/MenuScreen.jsx
+++ screens/MenuScreen.jsx
@@ -5,65 +5,42 @@ import useTextToSpeech from '../hooks/useTextToSpeech';
 import * as Speech from 'expo-speech';
 
 const MenuScreen = ({ navigation }) => {
-    const speakGame = useTextToSpeech('Double Tap to Start Game');
+    const doubleTapRef = useRef(false);
+
+    const speakAndNavigate = (text, screen, speakFunction, speakFunction2) => {
+        if (doubleTapRef.current) {
+            navigation.navigate(screen);
+            Speech.speak(text);
+        } else {
+            speakFunction2();
+            doubleTapRef.current = true;
+            setTimeout(() => {
+                doubleTapRef.current = false;
+            }, 500);
+        }
+    };
+
+    const speakGame = useTextToSpeech('game has started');
     const speakLeaderBoard = useTextToSpeech('Double Tap to Announce Leader Board');
     const speakResume = useTextToSpeech('Double Tap to Resume Saved Game');
 
     const speakGame2 = () => {
-        const thingToSay = 'Game Has Started';
+        const thingToSay = 'Double Tap to start game';
         Speech.speak(thingToSay);
-      };
-    const doubleTapRef = useRef(false);
+    };
+
     const speakLeaderBoard2 = () => {
-        const sayLeaderBoard = 'Announcing Leader Board';
+        const sayLeaderBoard = 'Double Tap to Announce Leader Board';
         Speech.speak(sayLeaderBoard);
     };
+
     const speakResume2 = () => {
-        const sayLeaderBoard = 'Resuming game';
+        const sayLeaderBoard = 'Double Tap to resume game';
         Speech.speak(sayLeaderBoard);
-    };
-
-    const handleButtonPressGame = () => {
-        if (doubleTapRef.current) {
-            navigation.navigate('Gameplay');
-            speakGame2();
-        } else {
-            speakGame();
-            doubleTapRef.current = true;
-            setTimeout(() => {
-                doubleTapRef.current = false;
-            }, 500);
-        }
-    };
-
-    const handleButtonPressLeaderBoard = () => {
-        if (doubleTapRef.current) {
-            navigation.navigate('Leaderboard');
-            speakLeaderBoard2();
-        } else {
-            speakLeaderBoard();
-            doubleTapRef.current = true;
-            setTimeout(() => {
-                doubleTapRef.current = false;
-            }, 500);
-        }
-    };
-
-    const handleButtonPressResume = () => {
-        if (doubleTapRef.current) {
-            navigation.navigate('Gameplay');
-            speakResume2();
-        } else {
-            speakResume();
-            doubleTapRef.current = true;
-            setTimeout(() => {
-                doubleTapRef.current = false;
-            }, 500);
-        }
     };
 
     // Announce screen changes for screen readers
-    React.useEffect(() => {
+    useEffect(() => {
         const screenChangeAnnouncement = "Menu Screen. Select an option to continue.";
         AccessibilityInfo.announceForAccessibility(screenChangeAnnouncement);
     }, []);
@@ -72,23 +49,23 @@ const MenuScreen = ({ navigation }) => {
         <View accessible={true} accessibilityLabel="Main Menu" style={styles.container}>
             <CustomButton
                 title="Start Game"
-                onPress={handleButtonPressGame}
+                onPress={() => speakAndNavigate('Game has started', 'Gameplay', speakGame, speakGame2)}
                 buttonStyle={styles.button}
-                textStyle={{ color: 'white', fontSize: 50 }}
+                textStyle={styles.buttonText}
                 accessibilityLabel="Start a new game"
             />
             <CustomButton
                 title="Leaderboard"
-                onPress={handleButtonPressLeaderBoard}
+                onPress={() => speakAndNavigate('Announcing Leader Board', 'Leaderboard', speakLeaderBoard, speakLeaderBoard2)}
                 buttonStyle={styles.button}
-                textStyle={{ color: 'white', fontSize: 50 }}
+                textStyle={styles.buttonText}
                 accessibilityLabel="View the leaderboard"
             />
             <CustomButton
                 title="Resume Saved Game"
-                onPress={handleButtonPressResume}
+                onPress={() => speakAndNavigate('Resuming Saved Game', 'Gameplay', speakResume, speakResume2 )}
                 buttonStyle={styles.button}
-                textStyle={{ color: 'white', fontSize: 50 }}
+                textStyle={styles.buttonText}
                 accessibilityLabel="Resume saved game"
             />
             {/* More menu items */}
@@ -100,12 +77,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'stretch', // Align items to take up the entire width
-        backgroundColor: '#1F1A38', // Dark purple
+        alignItems: 'stretch',
+        backgroundColor: '#1F1A38',
     },
     button: {
-        flex: 1, // Each button takes equal space
-        margin: 8, // Add some margin for better spacing
+        flex: 1,
+        margin: 8,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 50,
     },
 });
 
