e6223965de88989813fc18c2f266938a3c1bf948
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 23:06:59 -0800

    IMproved button styling

--- screens/styles/appStyles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const appStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    },
-});
