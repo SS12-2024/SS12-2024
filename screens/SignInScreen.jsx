413c737ad98f833759cd869d4782459e69137e67
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-02-03 21:39:28 -0800

    improved button functionality

--- screens/SignInScreen.jsx
+++ screens/SignInScreen.jsx
@@ -1,52 +1,40 @@
 import React, { useEffect, useRef } from 'react';
-import { View, Button, Text, AccessibilityInfo } from 'react-native';
+import { View, Text, AccessibilityInfo, TouchableOpacity } from 'react-native';
 import { styles } from './styles/MenuStyles.js'
-import CustomButton from '../components/common/CustomButton';
-import { appStyles } from './styles/appStyles.js';
+import { buttonStyle } from './styles/buttonStyle.js';
 import useTextToSpeech from '../hooks/useTextToSpeech';
 import * as Speech from 'expo-speech';
 
 const SignInScreen = ({ navigation }) => {
     // Announce screen changes for screen readers
-    const speak = useTextToSpeech('Start Tutorial');
+    const speak = useTextToSpeech('Long-press anywhere on the screen to start the tutorial');
     const speak2 = () => {
         const thingToSay = 'Tutorial has Started';
         Speech.speak(thingToSay);
     };
-    const doubleTapRef = useRef(false);
+   
+    const handleLongPress = () => {
+        navigation.navigate('Tutorial');
+        speak2();
+      };
 
-    const handleButtonPress = () => {
-        if (doubleTapRef.current) {
-            // On the second tap, navigate to the SignIn screen
-            navigation.navigate('Tutorial');
-            speak2();
-
-        } else {
-            // On the first tap, speak the accessibility label
-            speak();
-            doubleTapRef.current = true;
-
-            // Reset double tap after a short delay (adjust as needed)
-            setTimeout(() => {
-                doubleTapRef.current = false;
-            }, 500);
-        }
-    };
     useEffect(() => {
         const screenChangeAnnouncement = "Sign Screen";
         AccessibilityInfo.announceForAccessibility(screenChangeAnnouncement);
     }, []);
 
     return (
-        <View accessible={true} accessibilityLabel="Gameplay Screen" style={appStyles.container}>
-            <CustomButton
-                title="Go to Tutorial"
-                onPress={handleButtonPress}
-                buttonStyle={styles.button}
-                textStyle={{ color: 'white', fontSize: 50 }}
-                accessibilityLabel="Start tutorial"
-            />
-        </View>
+        <TouchableOpacity
+            accessible={true}
+            accessibilityLabel="Gameplay Screen"
+            onPress={speak}
+            onLongPress={handleLongPress}
+            style={buttonStyle.container}
+        >
+            <View>
+                <Text style={styles.button}>Go to Tutorial</Text>
+            </View>
+        </TouchableOpacity>
     );
 };
 
